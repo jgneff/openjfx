efbeb08be685d8a56d7e15d6896a650882a140af
John Neffenger <john@status6.com>
2022-06-02 16:11:34 -0700

    Create the environment file in SNAP_DATA directory
    Create the environment file in the directory defined by SNAP_DATA
    instead of SNAP_COMMON so that it will be correct after running the
    'snap revert' command. Unlike SNAP_COMMON, the SNAP_DATA directory
    is backed up and restored across 'snap refresh' and 'snap revert'
    operations. For more information, see:
    
    Environment variables
    https://snapcraft.io/docs/environment-variables

--- bin/envhook.sh
+++ bin/envhook.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
-# Creates or updates the environment file
+# Creates the environment file on installation and updates
 
 # Checks for required environment variables
-: "${SNAP_INSTANCE_NAME:?}"
-: "${SNAP_REVISION:?}"
-: "${SNAP_COMMON:?}"
-
-# Snaps are under '/var/lib/snapd/snap' on Fedora, '/snap' on Debian.
-root=/var/lib/snapd
-snap=/snap/${SNAP_INSTANCE_NAME}/${SNAP_REVISION}
-sdklib=/sdk/lib
+: "${SNAP:?}"
+: "${SNAP_DATA:?}"
 
 {
     printf "# Source this file for OpenJFX environment variables\n"
-    if [ -d "${root}${snap}${sdklib}" ]; then
-        printf "export JAVAFX_LIB=%s/sdk/lib\n" "${root}${snap}"
-        printf "export JAVAFX_MOD=%s/jmods\n" "${root}${snap}"
-    elif [ -d "${snap}${sdklib}" ]; then
-        printf "export JAVAFX_LIB=%s/sdk/lib\n" "${snap}"
-        printf "export JAVAFX_MOD=%s/jmods\n" "${snap}"
-    fi
-} > "${SNAP_COMMON}/openjfx.env"
+    printf "export JAVAFX_LIB=%s/sdk/lib\n" "$SNAP"
+    printf "export JAVAFX_MOD=%s/sdk/jmods\n" "$SNAP"
+} > "$SNAP_DATA/openjfx.env"
